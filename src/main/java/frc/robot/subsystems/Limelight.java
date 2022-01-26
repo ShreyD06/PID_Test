bbf4dcec78c443e8c5cc5ebbd861f6bd8529e9fa
Shrey D <shreyder.desai@gmail.com>
2022-01-25 19:07:16 -0500

    Added aim + shooter code

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -4,11 +4,38 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.LimelightConstants;
 
 public class Limelight extends SubsystemBase {
+  NetworkTable limelightTable;
+  double ty, tv, tx, angle, distance;
+
   /** Creates a new Limelight. */
-  public Limelight() {}
+  public Limelight() {
+    limelightTable = NetworkTableInstance.getDefault().getTable("limelight");
+    ty = limelightTable.getEntry("ty").getDouble(0);
+    tv = limelightTable.getEntry("tv").getDouble(0);
+    tx = limelightTable.getEntry("tv").getDouble(0);
+  }
+
+  public double getAngle() {
+    if (tx != 0) {
+      return tx;
+    }
+    return 0;
+  }
+
+  public double getDistance() {
+    if (tv != 0) {
+      angle = (LimelightConstants.ANGLE_CAMERA + ty) * Math.PI / 180;
+      distance = (LimelightConstants.HEIGHT_TARGET - LimelightConstants.HEIGHT_CAMERA) / Math.tan(angle);
+      return distance;
+    }
+    return 0;
+  }
 
   @Override
   public void periodic() {
--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -6,6 +6,7 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import frc.wpi.first.wpilibj.controller;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ShooterConstants;
@@ -13,7 +14,31 @@ import frc.robot.Constants.ShooterConstants;
 
 public class Shooter extends SubsystemBase {
   /** Creates a new Shooter. */
-  public Shooter() {}
+  CANSparkMax encoderTop, encoderBottom;
+  WPI_VictorSPX motorTop, motorBottom;
+  public Shooter() {
+    encoderTop = new CANSparkMax(5);
+    encoderBottom = new CANSparkMax(6);
+    motorTop = new WPI_VictorSPX(7);
+    motorBottom = new WPI_VictorSPX(8);
+
+    PIDController pid = new PIDController(constants.kP, constants.kI, constants.kD);
+
+
+    topShooterMotors = new SpeedControllerGroup(encoderTop, motorTop);
+    bottomShooterMotors = new SpeedControllerGroup(encoderBottom, motorBottom);
+  }
+
+  public void set(int setpoint) {
+    motorTop.set(pid.calculate(encodertop.getrate() * VEL_RPM_CONVERSION, setpoint));
+    motorBottom.set(pid.calculate(encoderbottom.getrate() * VEL_RPM_CONVERSION, setpoint));
+  }
+
+  public void stop()
+  {
+    topShooterMotors.set(0);
+    bottomShooterMotors.set(0);
+  }
 
   @Override
   public void periodic() {
